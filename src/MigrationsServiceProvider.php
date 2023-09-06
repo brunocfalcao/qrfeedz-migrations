985c201a5ac1f5120bbe359c8e66bddcda5609f1
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-06 22:45:42 +0200

    wip

--- src/MigrationsServiceProvider.php
+++ src/MigrationsServiceProvider.php
@@ -3,26 +3,16 @@
 namespace QRFeedz\Migrations;
 
 use QRFeedz\Foundation\Abstracts\QRFeedzServiceProvider;
-use QRFeedz\Migrations\Commands\FreshSeed;
 
 class MigrationsServiceProvider extends QRFeedzServiceProvider
 {
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        $this->registerCommands();
     }
 
     public function register()
     {
         //
-    }
-
-    protected function registerCommands()
-    {
-        $this->commands([
-            FreshSeed::class,
-        ]);
     }
 }
